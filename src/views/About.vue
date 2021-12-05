86f31b639e77e6af4a1a7eca0f2941b1d71d61f3
Daniel Chmur <daniel.chmur@gmail.com>
2021-12-05 17:45:25 +0100

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure saepe
+      commodi cupiditate eius omnis inventore est ea ipsum mollitia nisi beatae
+      dignissimos iusto laborum, hic officia, quisquam ratione tempore cum.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quis
+      commodi sequi quidem officiis vel nemo suscipit provident doloremque?
+      Quasi aliquam molestiae debitis perspiciatis dolore consectetur a placeat
+      explicabo eius.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+      doloremque optio illo quisquam perspiciatis repudiandae nulla error
+      veniam! Nostrum vitae maiores autem, et hic neque at quam porro ipsum
+      quis!
+    </p>
+  </div>
+</template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home pa-6">
+    <h1>Todo page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Todo",
+};
+</script>
